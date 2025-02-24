9259daabd79d256af361ff8847a4d4f308c86dae
mackhankins <mhankins@mdah.ms.gov>
2025-02-24 14:43:48 -0600

    site manifest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="{{ Vite::asset('resources/images/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ Vite::asset('resources/images/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ Vite::asset('resources/images/favicon-16x16.png') }}">
-    <link rel="manifest" href="./site.webmanifest">
+    <link rel="manifest" href="{{  asset('site.webmanifest') }}" name="site.webmanifest">
 </head>
 <body class="font-sans antialiased">
 {{ $slot }}
